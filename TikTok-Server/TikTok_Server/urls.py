d80dc736d3f6b0811fcc23836525e99372d218a4
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-11-29 18:29:07 -0500

    router videos implemented

--- TikTok-Server/TikTok_Server/urls.py
+++ TikTok-Server/TikTok_Server/urls.py
@@ -17,14 +17,12 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
-from django.urls import path,include
-from drf_yasg.views import get_schema_view
+from django.urls import include, path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
-
-
+from video.api.routes import router_video
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,12 +41,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger',
-            cache_timeout=0), name='schema-swagger-ui'),
+                                      cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
+                                        cache_timeout=0), name='schema-redoc'),
 
-    #Router de users
+    # Router de users
     path('api/', include('users.api.router')),
-   
+    # Router de videos
+    path('api/', include(router_video.urls)),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
