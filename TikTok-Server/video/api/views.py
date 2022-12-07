a24fc97c7593b62a25156489f8b257cf920943f3
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-07 12:15:29 -0500

    comment add

--- TikTok-Server/video/api/views.py
+++ TikTok-Server/video/api/views.py
@@ -2,7 +2,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
 from video.api.serializer import VideoSerializer
 from video.models import Video
 
@@ -13,7 +12,7 @@ class VideoApiViewSet(ModelViewSet):
 
     queryset = Video.objects.all()
     http_method_names = ['get', 'post']
-    # Filtering
+    # Indicator of libraries to use
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     # Filter by userId, the user who created the video
     filterset_fields = ['user']
