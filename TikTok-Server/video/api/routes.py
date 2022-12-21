98aed6e26ea4c41148f6d5b68a35e4242f958153
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-20 20:48:10 -0500

    New routes created

--- TikTok-Server/video/api/routes.py
+++ TikTok-Server/video/api/routes.py
@@ -1,8 +1,16 @@
 from rest_framework.routers import DefaultRouter
-
-from video.api.views import VideoApiViewSet
+from video.api.views import (VideoActionsApiViewSet, VideoApiViewSet,
+                             VideoLikeApiViewSet)
 
 router_video = DefaultRouter()
 
 router_video.register(prefix='video', basename='video',
                       viewset=VideoApiViewSet)
+
+
+router_video.register(prefix='video/actions', basename='video',
+                      viewset=VideoActionsApiViewSet)
+
+
+router_video.register(prefix='video_like', basename='video',
+                      viewset=VideoLikeApiViewSet)
