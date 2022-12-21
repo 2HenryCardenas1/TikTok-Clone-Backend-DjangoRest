582cd8b961058348c2c6893de7a960fcd419225b
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-20 20:45:56 -0500

    model user edited

--- TikTok-Server/users/models.py
+++ TikTok-Server/users/models.py
@@ -1,23 +1,24 @@
 import os
-from django.db import models
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-#Function to get the path of the image and save in the specific path
-def get_file_path(instance,filename):
+
+# Function to get the path of the image and save in the specific path
+def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
-    filename = "%s.%s"%(instance.id,ext)
-    return os.path.join('avatar/',filename)
+    filename = "%s.%s" % (instance.id, ext)
+    return os.path.join('avatar/', filename)
 
 
-# This models extend the default user model. 
+# This models extend the default user model.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    avatar =  models.ImageField(upload_to=get_file_path,blank=True)
-    description = models.CharField(max_length=100,blank=True)
-    website = models.CharField(max_length=100,blank=True)
-    instagram = models.CharField(max_length=100,blank=True)
+    avatar = models.ImageField(upload_to=get_file_path, blank=True)
+    description = models.CharField(max_length=100, blank=True)
+    website = models.CharField(max_length=100, blank=True)
+    instagram = models.CharField(max_length=100, blank=True)
 
     # Specific email to login in web, the default is a username.
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-     
