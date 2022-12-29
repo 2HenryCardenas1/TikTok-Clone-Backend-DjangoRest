5db2d9d3b9687241c3a80083841a88f13ead736c
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-29 16:57:35 -0500

    get list user and get user by id

--- TikTok-Server/users/api/router.py
+++ TikTok-Server/users/api/router.py
@@ -1,11 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
-from users.api.view import RegisterView, UserMeView
+from users.api.view import RegisterView, UserApiViewSet, UserMeView
+
+router_user = DefaultRouter()
+
+router_user.register(prefix='users', basename='users', viewset=UserApiViewSet)
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/me/', UserMeView.as_view())
+    path('user/me/', UserMeView.as_view()),
+
 ]
